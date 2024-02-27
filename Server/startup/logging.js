e5fdcfe5d04f218454455d480bd3f258085b55db
NoaReuven2308 <161506474+NoaReuven2308@users.noreply.github.com>
2024-02-27 23:30:16 +0200

    Update logging.js

--- Server/startup/logging.js
+++ Server/startup/logging.js
@@ -19,7 +19,7 @@ module.exports = function() {
   
   logger.add(new winston.transports.File({ filename: 'logfile.log' }));
   logger.add(new winston.transports.MongoDB({ // For Testing
-    db: 'mongodb://localhost/webCoding',
+    db: 'mongodb://mongo:611BGc5Hh5aC45bAg56-Cfe-14a4cgED@roundhouse.proxy.rlwy.net:13646',
     level: 'info'
   })); 
 }
