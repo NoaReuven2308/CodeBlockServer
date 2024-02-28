60ddebac2e997de93b4bcf4677316cfdf3233a36
NoaReuven2308 <161506474+NoaReuven2308@users.noreply.github.com>
2024-02-28 10:19:22 +0200

    Update validateObjectId.js

--- Server/middleware/validateObjectId.js
+++ Server/middleware/validateObjectId.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+ // Validate ID Middleware:
+ // Validates if the provided ID is a valid MongoDB ObjectId.
 module.exports = function(req, res, next) {
   if (!mongoose.Types.ObjectId.isValid(req.params.id))
     return res.status(404).send('Invalid ID.');
