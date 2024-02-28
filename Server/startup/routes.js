6d38403b6130d192a6a8c1d91903c4ca59349072
NoaReuven2308 <161506474+NoaReuven2308@users.noreply.github.com>
2024-02-28 10:23:17 +0200

    Update routes.js

--- Server/startup/routes.js
+++ Server/startup/routes.js
@@ -2,9 +2,18 @@ const express = require('express');
 const codeBlocks = require('../routes/codeBlocks');
 const error = require('../middleware/error');
 
-
+/**
+ * Express Middleware Setup:
+ * Configures middleware for handling JSON requests, routing for code blocks, and error handling.
+ * @param {Object} app - Express application object
+ */
 module.exports = function(app) {
+  // Parse JSON requests
   app.use(express.json());
+  
+  // Route requests for code blocks
   app.use('/api/codeblocks', codeBlocks);
+  
+  // Error handling middleware
   app.use(error);
 }
