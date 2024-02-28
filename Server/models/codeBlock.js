e4ef7ebe7aabd113237536aeb6a646582a6a8394
NoaReuven2308 <161506474+NoaReuven2308@users.noreply.github.com>
2024-02-28 10:20:01 +0200

    Update codeBlock.js

--- Server/models/codeBlock.js
+++ Server/models/codeBlock.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 
+/**
+ * CodeBlock Schema:
+ * Defines the structure of a code block document in MongoDB.
+ */
 const codeBlockSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -11,19 +15,29 @@ const codeBlockSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  
 });
 
+/**
+ * CodeBlock Model:
+ * Represents the MongoDB collection for code blocks.
+ */
 const CodeBlock = mongoose.model('CodeBlock', codeBlockSchema);
 
+/**
+ * Validate CodeBlock Function:
+ * Validates the properties of a code block object using Joi schema.
+ * @param {Object} codeBlock - The code block object to be validated
+ * @returns {Object} - Joi validation result object
+ */
 function validateCodeBlock(codeBlock) {
-    const schema = Joi.object({
-      title: Joi.string().min(1).max(50).required(),
-      code: Joi.string().optional().default('Empty Code'),
-    });
-  
-    return schema.validate(codeBlock);
+  const schema = Joi.object({
+    title: Joi.string().min(1).max(50).required(),
+    code: Joi.string().optional().default('Empty Code'),
+  });
+
+  return schema.validate(codeBlock);
 }
 
-exports.CodeBlock = CodeBlock; 
+// Exporting CodeBlock model and validation function
+exports.CodeBlock = CodeBlock;
 exports.validate = validateCodeBlock;
