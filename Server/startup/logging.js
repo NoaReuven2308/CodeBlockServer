cac320da7dc47b3a9fe7973c54b86822078c78b9
NoaReuven2308 <161506474+NoaReuven2308@users.noreply.github.com>
2024-02-28 10:22:42 +0200

    Update logging.js

--- Server/startup/logging.js
+++ Server/startup/logging.js
@@ -1,25 +1,36 @@
 const winston = require('winston');
-require('winston-mongodb'); // Beacuse Testing
+require('winston-mongodb');
 require('express-async-errors');
 
-
+/**
+ * Logging Setup:
+ * Configures Winston logger with file and MongoDB transports.
+ */
 module.exports = function() {
+  // Create a logger instance
   const logger = winston.createLogger({
     transports: [
+      // Log uncaught exceptions to a file
       new winston.transports.File({ filename: 'uncaughtExceptions.log' })
     ],
+    // Handle uncaught exceptions and log them to the same file
     exceptionHandlers: [
       new winston.transports.File({ filename: 'uncaughtExceptions.log' })
     ]
   });
   
+  // Throw unhandled rejections as exceptions to be caught by the logger
   process.on('unhandledRejection', (ex) => {
     throw ex;
   });
   
+  // Add file transport for regular logging
   logger.add(new winston.transports.File({ filename: 'logfile.log' }));
-  logger.add(new winston.transports.MongoDB({ // For Testing
+  
+  // Add MongoDB transport for testing purposes
+  logger.add(new winston.transports.MongoDB({
     db: 'mongodb://mongo:611BGc5Hh5aC45bAg56-Cfe-14a4cgED@roundhouse.proxy.rlwy.net:13646',
+    //db: 'mongodb://localhost/webCoding',
     level: 'info'
   })); 
 }
